6585320f212cd3e46c11e564799e8427037be9bc
Tierey <16826506+Tierey@users.noreply.github.com>
2024-01-29 05:05:13 +0300

    Update report.groovy

--- vars/report.groovy
+++ vars/report.groovy
@@ -1,18 +1,17 @@
 import java.io.*;
 import groovy.io.*;
+
 @NonCPS
 def call (Map config=[:]){
 
     def dir = new File(pwd());
     def writer = new File(dir.path + "/releasenotes.txt").newWriter()
  
-        writer -> dir.eachFileRecurse(FiletType.ANY) { file ->
-            if(file.isDirectory()){
-                writer.writeFile(file.name)
-            }else{
-                writer.writeFile("\t" + file.name + "\t" + file.length())
-            }
+    dir.eachFileRecurse(FiletType.ANY) { file ->
+        if(file.isDirectory()){
+            writer.writeFile(file.name)
+        }else{
+            writer.writeFile("\t" + file.name + "\t" + file.length())
         }
-    
-
+   }
 }
